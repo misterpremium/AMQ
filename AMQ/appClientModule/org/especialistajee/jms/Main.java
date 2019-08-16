ca6a7088bbcc53a7f18d284c9daff8e13d3762fe
misterpremium <simbacat@hotmail.es>
2019-08-16 09:50:49 +0200

    First commit

---
+++ AMQ/appClientModule/org/especialistajee/jms/Main.java
@@ -0,0 +1,28 @@
+package org.especialistajee.jms;
+
+/*
+import javax.annotation.Resource;
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageProducer;
+import javax.jms.Queue;
+import javax.jms.Session;
+*/
+// imports
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+    	Producctor p = new Producctor();
+        p.enviaMensajeCola("Hola Mundo");
+        p.enviaMensajeCola("Adios Mundo");
+        
+        ConsumidorSincrono c = new ConsumidorSincrono();
+        c.recibeMensajeSincronoCola();
+    
+    }
+
+	
+}
